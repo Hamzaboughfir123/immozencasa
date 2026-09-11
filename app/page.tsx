ee4895b7bce0354f339bed2332bfe54e284ffccc
Boughfir Hamza <hamzaboughfir12@gmail.com>
2026-09-11 12:42:42 +0100

    liens

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Footer } from "@/components/layout/Footer";
 import { MobileStickyCta } from "@/components/layout/MobileStickyCta";
 import { Navbar } from "@/components/layout/Navbar";
-import { AboutSection } from "@/components/sections/AboutSection";
 import { BenefitsSection } from "@/components/sections/BenefitsSection";
 import { FaqSection } from "@/components/sections/FaqSection";
 import { HeroSection } from "@/components/sections/HeroSection";
@@ -20,7 +19,6 @@ export default function Home() {
         <SavingsSection />
         <PropertyTypesSection />
         <HowItWorksSection />
-        <AboutSection />
         <FaqSection />
         <OwnerLeadSection />
       </main>
